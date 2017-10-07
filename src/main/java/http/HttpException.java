d6b8824515614932546ee00de847e7d575e7d9bb
Joaquin Zamora <joaquin_z619@hotmail.com>
2017-10-07 07:45:42 +0200

    copiado paquete http para simulaciones

---
+++ src/main/java/http/HttpException.java
@@ -0,0 +1,10 @@
+package http;
+
+public class HttpException extends RuntimeException {
+    private static final long serialVersionUID = -5216295603814526456L;
+
+    public HttpException(String msg) {
+        super(msg);
+    }
+
+}
