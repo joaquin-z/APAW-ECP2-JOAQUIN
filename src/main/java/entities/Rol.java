5bbf50e5d8d32eed43893021e12aa20f0ab12ff4
Joaquin Zamora <joaquin_z619@hotmail.com>
2017-10-07 09:10:55 +0200

    creadas las entidades user, rol y authentication

---
+++ src/main/java/entities/Rol.java
@@ -0,0 +1,8 @@
+package entities;
+
+public enum Rol {
+    ADMIN,
+    OPERATION,
+    MANAGER,
+    CUSTOMER;    
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,38 @@
+package entities;
+
+import java.util.Calendar;
+
+public class User {
+    
+    private int id;
+
+    private String name;
+
+    private Calendar birthday;
+
+    private boolean active;
+    
+    public User(int id, String name, Calendar birthday, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public boolean isActive() {
+        return active;
+    }    
+    
+}
